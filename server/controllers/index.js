375a86a1d441e2c72d56fc28ec1a79172e7cacb4
Boris Goranoff <bggoranoff@localhost.localdomain>
2020-05-27 17:47:19 +0300

    Feat: added stream controller functionality

---
+++ server/controllers/index.js
@@ -0,0 +1,7 @@
+const category = require('./category');
+const movie = require('./movie');
+
+module.exports = {
+    movie,
+    category
+};
--- server/controllers/movie.js
+++ server/controllers/movie.js
@@ -1,12 +1,18 @@
 const models = require('../models');
 const helpers = require('../utils/helpers');
+const fs = require('fs');
 
 module.exports = {
     get: (req, res, next) => {
         models.Movie.find().populate('category')
             .then(movies => res.send(movies))
             .catch(next);
-        return;
+    },
+    getByCategory: (req, res, next) => {
+        const { categoryId } = req.params;
+        models.Movie.find({ category: categoryId }).populate('category')
+            .then(movies => res.send(movies))
+            .catch(next);
     },
     post: async (req, res, next) => {
         const { title, description, category } = req.body;
@@ -34,5 +40,38 @@ module.exports = {
                 res.send(deletedMovie);
             })
             .catch(next);
+    },
+    stream: async (req, res, next) => {
+        const { movieId } = req.params;
+        const movie = await models.Movie.findOne({ _id: movieId })
+            .then(movie => movie.json());
+        const filePath = movie.filePath;
+        const stat = fs.statSync(filePath);
+        const fileSize = stat.size;
+        const range = req.headers.range;
+
+        if(range) {
+            const parts = range.replace(/bytes=/, "").split("-");
+            const start = parseInt(parts[0], 10);
+            const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+            const chunksize = (end - start) + 1;
+            const file = fs.createReadStream(filePath, { start, end });
+            const head = {
+                'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+                'Accept-Ranges': 'bytes',
+                'Content-Length': chunksize,
+                'Content-Type': 'video/mp4'
+            };
+
+            res.writeHead(206, head);
+            file.pipe(res);
+        } else {
+            const head = {
+                'Content-Length': fileSize,
+                'Content-Type': 'video/mp4'
+            };
+            res.writeHead(200, head);
+            fs.createReadStream(filePath).pipe(res);
+        }
     }
-}
+};
