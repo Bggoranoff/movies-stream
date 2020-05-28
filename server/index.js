b6dba24669a45228e71954fd07f1b3845b53e6bc
Boris Goranoff <bggoranoff@localhost.localdomain>
2020-05-28 14:33:14 +0300

    Fix: express connection

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
 const config = require('./config/config');
 const dbConnection = require('./config/database');
 
-const app = require('express');
+const app = require('express')();
 
 dbConnection().then(() => {
     require('./config/express')(app);
-    // TODO: require routes
+    require('./config/routes')(app);
 
-    app.request(function(err, req, res, next) {
+    app.use(function(err, req, res, next) {
         console.error(err);
         res.status(500).send(err.message);
     });
